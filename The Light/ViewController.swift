ec48f6b13b22a8cbd43b7a0109302e6a59e0d5e9
Saliq <saliq.ilshat@icloud.com>
2020-10-12 11:41:13 +0500

    Convert project from The Light to The Rainbow Add app icons

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -8,28 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var isLight = true
+    var colorRainbow = 0
+    
     override var prefersStatusBarHidden: Bool{
         return true
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        changeLight()
+        changeColor()
         // Do any additional setup after loading the view.
     }
 
 
-    fileprivate func changeLight() {
-        if isLight {
-            view.backgroundColor = .white
-        } else{
-            view.backgroundColor = .black
+    fileprivate func changeColor() {
+        
+        switch colorRainbow {
+        case 0: view.backgroundColor = .red
+        case 1: view.backgroundColor = .orange
+        case 2: view.backgroundColor = .yellow
+        case 3: view.backgroundColor = .green
+        case 4: view.backgroundColor = .cyan
+        case 5: view.backgroundColor = .blue
+        case 6: view.backgroundColor = .purple
+        default:
+            view.backgroundColor = .red
         }
+        
+        //Если счетчик становиться равным 7 то обнулим его
+        colorRainbow += 1
+        if colorRainbow == 7 {
+            colorRainbow = 0
+        }
+        
     }
     
     @IBAction func buttonPressed() {
-        isLight.toggle()
-        changeLight()
+        changeColor()
     }
 }
 
